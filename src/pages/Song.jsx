7885f147e1333dd32b96b4aacda56861a4a32368
magnosansil <magnosansil@outlook.com>
2025-02-12 21:42:35 -0300

    Creates Song Page

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import Player from "../components/Player";
+import { Link, useParams } from "react-router-dom";
+import { songsArray } from "../assets/database/songs";
+import { artistArray } from "../assets/database/artists";
 
 const Song = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { id } = useParams();
 
-export default Song
+  const { image, name, duration, artist, audio, index } = songsArray.filter(
+    (currentSongObj) => currentSongObj.id === Number(id)
+  )[0];
+
+  const artistObj = artistArray.filter(
+    (currentArtistObj) => currentArtistObj.name === artist
+  )[0];
+
+  const songsArrayFromArtist = songsArray.filter(
+    (currentSongObj) => currentSongObj.artist === artist
+  );
+
+  console.log(songsArrayFromArtist);
+
+  const randomIndex = Math.floor(
+    Math.random() * (songsArrayFromArtist.length - 1)
+  );
+
+  const randomIdFromArtist = songsArrayFromArtist[randomIndex].id;
+  const randomIdFromArtist2 = songsArrayFromArtist[randomIndex].id;
+
+  return (
+    <div className="song">
+      <div className="song__container">
+        <img src={image} alt={`Imagem da Música ${name}`} />
+      </div>
+      <div className="song__bar">
+        <Link to={`/artist/${artistObj.id}`} className="song__artist-image">
+          <img
+            className="song__artist-image"
+            src={artistObj.image}
+            alt={`Imagem do Artista ${artist}`}
+            width={75}
+            height={75}
+          />
+        </Link>
+        <Player
+          duration={duration}
+          randomIdFromArtist={randomIdFromArtist}
+          randomIdFromArtist2={randomIdFromArtist2}
+        />
+        <div>
+          <p className="song__name">{name}</p>
+          <p>{artist}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Song;
