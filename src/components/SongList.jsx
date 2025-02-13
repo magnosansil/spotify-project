782cc2c35c79f2c3914c4f1dab7bc75f12132805
magnosansil <magnosansil@outlook.com>
2025-02-12 21:42:20 -0300

    Creates Songs List Page

---
+++ src/components/SongList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState } from "react";
+import SongItem from "./SongItem";
+
+const SongList = ({ songsArray }) => {
+  const [items, setItems] = useState(5);
+  return (
+    <div className="song-list">
+      {songsArray
+        .filter((_currentValue, index) => index < items)
+        .map((currentSongObj, index) => (
+          <SongItem {...currentSongObj} index={index} key={index} />
+        ))}
+
+      <p
+        className="song-list__see-more"
+        onClick={() => {
+          setItems(items + 5);
+        }}
+      >
+        See More
+      </p>
+    </div>
+  );
+};
+
+export default SongList;
