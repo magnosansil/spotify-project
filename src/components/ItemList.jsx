2ef8d00566611fa63818b53d894f99bb2ba5e41a
magnosansil <magnosansil@outlook.com>
2025-02-12 21:42:48 -0300

    Creates Artist Page

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import SingleItem from "./SingleItem";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+  const { pathname } = useLocation();
+  const isHome = pathname === "/";
+  let totalItems = isHome ? items : Infinity;
+
   return (
     <div className="item-list">
       <div className="item-list__header">
         <h2>Popular {title}</h2>
-        <Link className="item-list__link" to={path}>
-          Show All
-        </Link>
+        {isHome ? (
+          <Link className="item-list__link" to={path}>
+            Show All
+          </Link>
+        ) : (
+          <></>
+        )}
       </div>
 
       <div className="item-list__container">
         {itemsArray
-          .filter((_, index) => index < items)
+          .filter((_, index) => index < totalItems)
           .map((item, index) => (
             <SingleItem {...item} key={`${title}-${index}`} idPath={idPath} />
           ))}
